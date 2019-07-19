4eb8db68e21ce3bfe6f371dc2f133a3a56596103
atanu-basu <atanubasu.21@gmail.com>
2019-07-19 12:42:11 +0530

    issue fixed image rendering in html page

--- Ads/server/routes/fetch.js
+++ Ads/server/routes/fetch.js
@@ -3,29 +3,22 @@ var Ads = require('../models/ad');
 var router = express.Router();
 
 var img;
-router.get('/' ,(req,res) => {
+router.get('/', (req, res) => {
     Ads.find({}).then((ads) => {
-       res.json(ads);
-    }, (err) => {
-        throw err;
-    });
-});
-router.get('/img' ,(req,res) => {
-    Ads.find({}).then((ads) => {
-       img = ads.image;
-       res.send(img);
+        res.json(ads);
+        // res.json(ads[0].image);
     }, (err) => {
         throw err;
     });
 });
 
-router.get('/:name', (req,res) => {
-        var name = req.params.name;
-        Ads.find({productDetails: name}).then((ads) => {
-            res.json(ads);
-        }, (err) => {
-            throw err;
-        });
+router.get('/:name', (req, res) => {
+    var name = req.params.name;
+    Ads.find({ productDetails: name }).then((ads) => {
+        res.json(ads);
+    }, (err) => {
+        throw err;
+    });
 });
 
 module.exports = router;
--- Ads/server/server.js
+++ Ads/server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
 var cors = require('cors');
+var path = require('path');
 
 
 mongoose.Promise = global.Promise;
@@ -17,10 +18,16 @@ var app = express();
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(cors());
+
 var create = require('./routes/create');
 app.use('/api/areacode/storename/offers/create', create);
 
 var fetch = require('./routes/fetch');
 app.use('/api/areacode/storename/offers/fetch', fetch);
+
+app.get('/:name', (req,res) => {
+    var name = req.params.name;
+    res.sendFile(path.join(__dirname+'/public/files/'+name));
+});
 
 app.listen(8080, () => console.log('server running on port 8080'));
